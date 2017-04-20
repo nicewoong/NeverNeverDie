16b9876f759a9e33abb41cd32650c8bbdb7f5103
nicewoong <thoughtscode@gmail.com>
2017-04-20 21:29:45 +0900

    Add Libraries - Retrofit, GSON at bulid.gradle(module)

--- app/src/main/java/com/nicewoong/neverneverdie/MainActivity.java
+++ app/src/main/java/com/nicewoong/neverneverdie/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    Pulbic data API KEY (for "trafficAccidentDeath")
+    PYWezzCNSGTHF5aJALsLoxzCziUi5d7B1jEPrU87TacdNZQoVimSQG%2FFBafP1RRBRZa1XVGX5babWe7wwSXr%2FQ%3D%3D
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
