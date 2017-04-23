b990257641236e74eb8fd96e202e180027c4846f
nicewoong <thoughtscode@gmail.com>
2017-04-23 17:00:46 +0900

    Add Listener on <button_check_around_me> and Created <CheckAroundMeMapActivity >

--- app/src/main/java/com/nicewoong/neverneverdie/ui/CheckAroundMeMapActivity.java
+++ app/src/main/java/com/nicewoong/neverneverdie/ui/CheckAroundMeMapActivity.java
@@ -4,21 +4,37 @@ import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nicewoong.neverneverdie.R;
 
-public class CheckAroundMeMapActivity extends FragmentActivity {
+public class CheckAroundMeMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_around_me_map);
 
+        Log.d(MainActivity.TAG_PROCEDURE_DEBUG,"CheckAroundMapActivity 가 열렸습니다");
+
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync();
-
-
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        Log.d(MainActivity.TAG_PROCEDURE_DEBUG,"맵이 준비되었습니다 ~!");
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("hello!"));
+    }
+
+
 }
--- app/src/main/java/com/nicewoong/neverneverdie/ui/MainActivity.java
+++ app/src/main/java/com/nicewoong/neverneverdie/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.nicewoong.neverneverdie.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.nicewoong.neverneverdie.R;
 import com.nicewoong.neverneverdie.trafficAccidentDeath.TrafficAccidentDeathDataCom;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     /*
     Pulbic data API KEY (for "trafficAccidentDeath")
@@ -20,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     public static String TAG_PROCEDURE_DEBUG  = "TAG_PROCEDURE_DEBUG";
     public static String TAG_REST_API_TEST  = "TAG_REST_API_TEST";
 
+    /*
+    button
+     */
+    public Button checkAroundMeButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +37,42 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG_PROCEDURE_DEBUG,"Start of onCreate()");
 
-        TrafficAccidentDeathDataCom trafficAccidentDeathDataCom = new TrafficAccidentDeathDataCom(getApplicationContext());
-        trafficAccidentDeathDataCom.setTrafficAccidentDeathList(TRAFFIC_ACCIDENT_DEATH_API_KEY,"2014","2200","2204");
+//        TrafficAccidentDeathDataCom trafficAccidentDeathDataCom = new TrafficAccidentDeathDataCom(getApplicationContext());
+//        trafficAccidentDeathDataCom.setTrafficAccidentDeathList(TRAFFIC_ACCIDENT_DEATH_API_KEY,"2014","2200","2204");
+
+        // Button Register
+        checkAroundMeButton = (Button)findViewById(R.id.button_check_around_me);
+        checkAroundMeButton.setOnClickListener(this); // Register onClickListener
+    }
 
 
+    /**
+     * Called when the user clicks the <Check around me> button
+     *
+     */
+    public void openCheckAroundMeMapActivity() {
+        Intent intent = new Intent(this, CheckAroundMeMapActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
+    /**
+     * Click Listener
+     * Called when user click the buttons and other items..
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        Log.d(MainActivity.TAG_PROCEDURE_DEBUG,"onClick() 호출되었습니다 ");
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.button_check_around_me:
+                Log.d(MainActivity.TAG_PROCEDURE_DEBUG,"Check around me 버튼을 클릭했습니다");
+                openCheckAroundMeMapActivity();
+                break;
+        }
     }
 }
