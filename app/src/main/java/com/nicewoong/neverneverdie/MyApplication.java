e6b34843a1f232fbf923714bcc85092af7bec18a
nicewoong <thoughtscode@gmail.com>
2017-04-23 22:24:07 +0900

    Created Application class

---
+++ app/src/main/java/com/nicewoong/neverneverdie/MyApplication.java
@@ -0,0 +1,39 @@
+package com.nicewoong.neverneverdie;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * Created by nicewoong on 2017. 4. 23..
+ */
+
+public class MyApplication extends Application {
+
+
+    public static GoogleApiClient mGoogleApiClient;
+
+    /** onCreate()
+     * 액티비티, 리시버, 서비스가 생성되기전 어플리케이션이 시작 중일때
+     * Application onCreate() 메서드가 만들어 진다고 나와 있습니다.
+     * by. Developer 사이트
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create an instance of GoogleAPIClient.
+    }
+
+    /**
+     * onConfigurationChanged()
+     * 컴포넌트가 실행되는 동안 단말의 화면이 바뀌면 시스템이 실행 한다.
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+}
